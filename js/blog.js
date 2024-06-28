a053aaba8eb54719190127c29218339731368791
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-27 20:32:39 -0400

    created blog framework

---
+++ js/blog.js
@@ -0,0 +1,16 @@
+const entries = JSON.parse(localStorage.getItem('blogData')) || []
+
+function displayEntries() {
+  const blogList = document.getElementById('blogEntries')
+  blogList.innerHTML = ''
+  for (const entry of entries) {
+    blogList.insertAdjacentHTML('beforeend', `
+    <article>
+      <header> <h2>${entry.title}</h2> <h3>${entry.username}</h3> </header>
+      <p>${entry.content}</p>
+<article>
+`)
+  }
+}
+
+displayEntries();
